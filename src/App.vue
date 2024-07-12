e33bbf858754c03e65f23dd58d56c7e0dbb4df70
oscarmauriciogiraldo <oscardan@utp.edu.co>
2024-07-11 23:16:53 -0500

    feat: single file component

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,23 @@
 <template>
   <h1>Hola invitacion</h1>
+  <section>
+    <h3>
+      counter: {{ 10 }}
+    </h3>
+    <h3>squere {{ 10 * 10 }}</h3>
+
+    <div class="containerButton">
+
+    </div>
+  </section>
 </template>
+
+<script lang="ts" setup>
+console.log('Hola SFC single file component')
+</script>
+
+<style scoped>
+h1 {
+  color: #B38D6A;
+}
+</style>
