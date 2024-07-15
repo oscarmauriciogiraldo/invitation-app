13a96b463289c5374705e917fe75ff4e8e5e981e
oscarmauriciogiraldo <oscardan@utp.edu.co>
2024-07-14 22:23:28 -0500

    refactor: separando por componentes globales

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
-  <h1>Chat app </h1>
+  <!-- <h1>Chat app </h1>
   <hr>
   <h1 className="text-3xl font-bold underline">
     Hello world!
-  </h1>
+  </h1> -->
+
+  <IndesicionView />
 
 
 </template>
 
 <script lang="ts" setup>
-
+import IndesicionView from '@/views/IndecicionView.vue';
 
 </script>
