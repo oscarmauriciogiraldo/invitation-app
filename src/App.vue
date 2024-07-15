84816c4589ee94c2471a0b09522b9cdd3d0ae480
oscarmauriciogiraldo <oscardan@utp.edu.co>
2024-07-14 21:56:13 -0500

    feat: instalacion Tailwind css

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,14 @@
 <template>
-  <h1>componente </h1>
+  <h1>Chat app </h1>
   <hr>
-  <!-- <WelcomComponent /> -->
-  <Mycounter :value="5" title="Comunicacion entre componente" />
-  <MycounterScript :value="10" title="Comunicacion entre componente props 2" />
-  <MyCounterScript2 :value="25" title="Comunicacion entre componente props 3" :nameBtn="nameBtn" />
+  <h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1>
 
 
 </template>
 
 <script lang="ts" setup>
-import Mycounter from './components/MyCounter.vue'
-import MycounterScript from './components/MyCounterScript.vue'
-import MyCounterScript2 from './components/MyCounterScript2/MyCounterScript2.vue';
-/* import WelcomComponent from './components/welcome-component/WelcomComponent.vue'; */
 
-const nameBtn = 'Boton ruta'
 
 </script>
